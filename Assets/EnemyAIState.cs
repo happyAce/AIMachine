b4d87ef5fdc560348d81a39b3be151c35b728eff
xianglijun <275923192@qq.com>
2018-12-03 18:56:00 +0800

    add

---
+++ Assets/EnemyAIState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace xlj
+{
+    public class Attack : State<EnemyTank>
+    {
+        public override void Enter(EnemyTank entity_type)
+        {
+            Vector3 targetPos = entity_type.Gettarget().transform.position;
+            path.InitByNavMeshPath(transform.position, targetPos);
+        }
+        public override void Execute(EnemyTank entity_type)
+        {
+            
+        }
+        public override void Exit(EnemyTank entity_type)
+        {
+            
+        }
+    }
+    public class Patrol : State<EnemyTank>
+    {
+        public override void Enter(EnemyTank entity_type)
+        {
+
+        }
+        public override void Execute(EnemyTank entity_type)
+        {
+
+        }
+        public override void Exit(EnemyTank entity_type)
+        {
+
+        }
+
+    }
+}
